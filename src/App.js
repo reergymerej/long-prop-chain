30a2ee0176ae77421a09cf825c0cace584526f61
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-03-08 08:07:55 -0600

    add redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Left from './Left'
 import Right from './Right'
+
+const reducer = (state = {}, action) => {
+  if (action.type === 'SHOW_CONTEXT') {
+    return { showContext: true }
+  }
+  return state
+}
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Left />
-        <Right />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Left />
+          <Right />
+        </div>
+      </Provider>
     );
   }
 }
